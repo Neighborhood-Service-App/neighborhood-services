d68aa40fd62c7a122497bb0ba33d945c89c5e99a
Dmytro Drahan <dmitrijrolkin@gmail.com>
2024-10-10 21:15:56 +0100

    Fix: added eureka client to user service. Tested: authentication flow with keycloak

--- back-end/services/user/src/main/java/com/neighborhoodservice/user/controller/UserController.java
+++ back-end/services/user/src/main/java/com/neighborhoodservice/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.neighborhoodservice.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
+
 
 
 
