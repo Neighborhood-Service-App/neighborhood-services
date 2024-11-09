914060eecc0c1ca8e7fd8de4ad223131ece567d4
Dmytro Drahan <dmitrijrolkin@gmail.com>
2024-11-09 16:04:08 +0000

    Added Response Status to AutorizationException

--- back-end/services/user/src/main/java/com/neighborhoodservice/user/exception/AuthorizationException.java
+++ back-end/services/user/src/main/java/com/neighborhoodservice/user/exception/AuthorizationException.java
@@ -1,5 +1,8 @@
 package com.neighborhoodservice.user.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = org.springframework.http.HttpStatus.FORBIDDEN)
 public class AuthorizationException extends RuntimeException {
 
     public AuthorizationException(String message) {
