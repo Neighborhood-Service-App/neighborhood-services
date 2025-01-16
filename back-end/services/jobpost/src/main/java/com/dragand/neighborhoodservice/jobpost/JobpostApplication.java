c40ea11cacd7e3ad4fb1e320536b45e925fc073e
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-01-16 19:50:53 +0000

    Initial JobPost service commit

---
+++ back-end/services/jobpost/src/main/java/com/dragand/neighborhoodservice/jobpost/JobpostApplication.java
@@ -0,0 +1,13 @@
+package com.dragand.neighborhoodservice.jobpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobpostApplication.class, args);
+	}
+
+}
---
+++ back-end/services/jobpost/src/test/java/com/dragand/neighborhoodservice/jobpost/JobpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragand.neighborhoodservice.jobpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
